479fa1aa7e19e609a3da280ca2d18ceef8281cbc
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-06-12 13:49:51 +0500

    first commit

---
+++ api/Index.js
@@ -0,0 +1,36 @@
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/userRoutes');
+const applyRoutes = require('./routes/applyRoutes');
+const app = express();
+
+// DB Connection
+mongoose.connect('mongodb+srv://nihaarshad5:r6eH4cYY4ZdOprgl@cluster0.o8bu9nt.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://fvg-global-assist.webflow.io/',
+  credentials: true
+}));
+
+
+
+
+// Routes
+app.use('/api', userRoutes);
+app.use('/apply', applyRoutes);
+// Start server
+app.listen(3000, () => console.log("Server running on port 3000"));
